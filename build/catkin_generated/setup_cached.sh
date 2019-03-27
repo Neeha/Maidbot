dc43d738a2d3e23103a622378078dea9418d8e1d
Neeha <neeha2311@gmail.com>
2019-03-27 14:38:18 -0500

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/neeha/meeeeeeee/jobs/Maidbot/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/neeha/meeeeeeee/jobs/Maidbot/catkin_ws/build"
+export PYTHONPATH="/home/neeha/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages:/usr/local/lib/python2.7/site-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/neeha/meeeeeeee/jobs/Maidbot/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/neeha/meeeeeeee/jobs/Maidbot/catkin_ws/src:$ROS_PACKAGE_PATH"
